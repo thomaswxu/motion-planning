19868e1024befe4051185816c237f0c844bae8e0
Tom Xu <thomas.w.xu@gmail.com>
2023-07-05 14:00:34 -0400

    Add initial PosePoints class files

---
+++ data_types/pose_points.hpp
@@ -0,0 +1,27 @@
+/**
+ * Class to represent an arm pose in terms of the Cartesian positions of each
+ * of its joints.
+ */
+#pragma once
+
+#include <vector>
+
+#include "edge.hpp"
+#include "pose.hpp"
+#include "vec3.hpp"
+
+class PosePoints
+{
+public:
+
+private:
+  const Pose pose_;
+  // const ArmDimensions arm_dimensions_;
+
+  const std::vector<Vec3> joint_points_; // Each joint XYZ, in ascending order from arm base
+  const std::vector<Edge> joint_edges_;  // Each link as an edge, in ascending order from arm base
+
+  Edge EE_edge_;
+  float EE_length_mm_;
+  float EE_radius_mm_;
+};
